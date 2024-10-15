c757b510f0ab527de11fd4db6bce2d07eb369fa4
joy-joy-joy-joy-joy-joy-joy <2890594946@qq.com>
2024-10-15 08:16:16 +0800

    update params

--- contract/src/router.sol
+++ contract/src/router.sol
@@ -8,6 +8,20 @@ contract UserTransactionRouter {
     address public dexAddress;
     uint256[] public pendingBlocks;
 
+    struct SwapParams {
+        uint256 amountIn;
+        uint256 amountOutMin;
+        address tokenIn;
+        address tokenOut;
+        address to;
+    }
+
+    struct PendingBlockInfo {
+        uint256 blockNumber;
+        uint256 transactionCount;
+        SwapParams[] swapParams;
+    }
+
     constructor(address _transactionPool, address _dexAddress) {
         transactionPool = TransactionPool(_transactionPool);
         dexAddress = _dexAddress;
@@ -64,43 +78,83 @@ contract UserTransactionRouter {
     function getValidPendingBlocks()
         public
         view
-        returns (uint256[] memory validBlocks, uint256[] memory txCounts)
+        returns (PendingBlockInfo[] memory)
     {
         uint256 validCount = 0;
         for (uint i = 0; i < pendingBlocks.length; i++) {
-            uint256 txCount = transactionPool
-                .getTransactions(pendingBlocks[i])
-                .length;
-            if (txCount > 0) {
+            TransactionPool.Transaction[] memory txs = transactionPool
+                .getTransactions(pendingBlocks[i]);
+            if (txs.length > 0) {
                 validCount++;
             }
         }
 
-        validBlocks = new uint256[](validCount);
-        txCounts = new uint256[](validCount);
+        PendingBlockInfo[] memory blockInfos = new PendingBlockInfo[](
+            validCount
+        );
         uint256 index = 0;
         for (uint i = 0; i < pendingBlocks.length; i++) {
-            uint256 txCount = transactionPool
-                .getTransactions(pendingBlocks[i])
-                .length;
-            if (txCount > 0) {
-                validBlocks[index] = pendingBlocks[i];
-                txCounts[index] = txCount;
+            TransactionPool.Transaction[] memory txs = transactionPool
+                .getTransactions(pendingBlocks[i]);
+            if (txs.length > 0) {
+                SwapParams[] memory swapParamsArray = new SwapParams[](
+                    txs.length
+                );
+                uint256 swapCount = 0;
+                for (uint j = 0; j < txs.length; j++) {
+                    if (
+                        keccak256(bytes(txs[j].operationName)) ==
+                        keccak256(bytes("swap"))
+                    ) {
+                        (
+                            uint256 amountIn,
+                            uint256 amountOutMin,
+                            address tokenIn,
+                            address tokenOut,
+                            address to
+                        ) = abi.decode(
+                                txs[j].params,
+                                (uint256, uint256, address, address, address)
+                            );
+                        swapParamsArray[swapCount] = SwapParams(
+                            amountIn,
+                            amountOutMin,
+                            tokenIn,
+                            tokenOut,
+                            to
+                        );
+                        swapCount++;
+                    }
+                }
+                assembly {
+                    mstore(swapParamsArray, swapCount)
+                }
+                blockInfos[index] = PendingBlockInfo(
+                    pendingBlocks[i],
+                    txs.length,
+                    swapParamsArray
+                );
                 index++;
             }
         }
+        return blockInfos;
     }
 
     function getEarliestPendingBlock()
         external
         view
-        returns (uint256 blockNumber, uint256 txCount)
+        returns (PendingBlockInfo memory)
     {
-        (
-            uint256[] memory validBlocks,
-            uint256[] memory txCounts
-        ) = getValidPendingBlocks();
-        require(validBlocks.length > 0, "no valid pending blocks");
-        return (validBlocks[0], txCounts[0]);
+        PendingBlockInfo[] memory blockInfos = getValidPendingBlocks();
+        require(blockInfos.length > 0, "no valid pending blocks");
+
+        PendingBlockInfo memory earliestBlock = blockInfos[0];
+        for (uint i = 1; i < blockInfos.length; i++) {
+            if (blockInfos[i].blockNumber < earliestBlock.blockNumber) {
+                earliestBlock = blockInfos[i];
+            }
+        }
+
+        return earliestBlock;
     }
 }
